43a4defb28ef46ae8a62193d7bd749b91a6c0b7d
zainab ali <zali48822@gmail.com>
2022-12-17 05:13:10 -0800

    negative positive

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+i = random.randint(-10, 1)
+if i>0:
+    print(f"{i}is positive")
+    elif i==0:
+        print(f"{i}is zero")
+    else:
+        print(f"{i}is negative")
+
