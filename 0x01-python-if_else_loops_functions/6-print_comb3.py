d35ed4a5415ec9d397bffd77b92f462b49e4f454
zainab ali <zali48822@gmail.com>
2022-12-18 23:32:27 -0800

    combination of number

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 10):
-for i in range(number + 1, 10):
+    for i in range(number + 1, 10):
     if number == 8 and i == 9:
         print("{}{}".format(number, i))
     else:
