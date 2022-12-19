8dab669cef2bfc6e0212e2f46f8867c5ff8d726a
zainab ali <zali48822@gmail.com>
2022-12-19 05:01:14 -0800

    fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 def fizzbuzz():
+    number = ""
     for i in range(1, 101):
-        if i == i*3
-        print("fizz")
-    elif i ==i*5
-    print("buzz")
-else:
-    print("fizzbuzz:)
-
+        if i % 3 == 0 and i % 5 == 0:
+            number = "FizzBuzz"
+        elif i % 3 == 0:
+            number = "Fizz"
+        elif i % 5 == 0:
+            number = "Buzz"
+        else:
+            number = f"{i}"
+            if i < 100:
+                print("{} ".format(number), end="")
+            else:
+                print("{} ".format(number), end="")
