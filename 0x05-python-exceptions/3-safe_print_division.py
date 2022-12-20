463b67473dceec12c59bcb1fe7314ab88944810d
zainab ali <zali48822@gmail.com>
2022-12-20 03:49:11 -0800

    division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("{:d}".format(result[i]), end="")
+        print("Inside result: {}".format(result))
         return (result)
