600f8b932516d2998fd00e7fa9ee98cf3cc89162
zainab ali <zali48822@gmail.com>
2022-12-21 09:20:51 -0800

    area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,8 +5,8 @@
 class Square():
     """creating the init function"""
     def __init__(self, size=0):
-        """ initializing the variable size and making it private attribute
-
+        """ initializing the variable size and setting it to a
+        private attribute. Also checking for typeError and value errors
         Args:
         size(int): the size of the square
         """
