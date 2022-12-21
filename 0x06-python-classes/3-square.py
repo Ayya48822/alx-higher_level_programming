5046460dabae114d0c294733ccb0688f048c9ee2
zainab ali <zali48822@gmail.com>
2022-12-21 10:46:37 -0800

    area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""creating the Square class"""
+""" creating a Square class"""
 
 
 class Square():
     """creating the init function"""
-    
+
     def __init__(self, size=0):
-        """ initializing the variable size and making it
-        private attribute
-        
+        """ initializing the size attribute
+        and setting it to private
+
         Args:
             size(int): the size of the square
         """
-        if type(size) is not int:
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif (size < 0):
             raise ValueError("size must be >= 0")
         self.__size = size
 
