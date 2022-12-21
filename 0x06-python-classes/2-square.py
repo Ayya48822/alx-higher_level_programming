080c3583c4f11acd765ba035708020ec53482366
zainab ali <zali48822@gmail.com>
2022-12-21 10:21:54 -0800

    size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,13 +5,13 @@
 class Square():
     """ initiallizing init function """
     
-    def _int_(self, size=0):
-    """ initiallizing size attribute and making  it to private
-    Args:
-    size(int): the size of the square
-    """
-    if (type(size) is not int):
-        raise TypeError("size must be an integer")
-    elif (size < 0):
-        raise ValueError("size must be >= 0")
-    self.__size = size
+        def _int_(self, size=0):
+        """ initiallizing size attribute and making  it to private
+        Args:
+        size(int): the size of the square
+        """
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
