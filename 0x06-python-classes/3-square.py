3f8fe0fed6dfcbb3d6f2f28ea41aa1d1010b98d3
zainab ali <zali48822@gmail.com>
2022-12-21 09:23:58 -0800

    area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,8 +7,9 @@ class Square():
     def __init__(self, size=0):
         """ initializing the variable size and setting it to a
         private attribute. Also checking for typeError and value errors
+
         Args:
-        size(int): the size of the square
+            size(int): the size of the square
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
