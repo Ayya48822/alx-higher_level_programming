10177c11c14f3c6ae7b1451ce54d9cd8c95ed6f4
zainab ali <zali48822@gmail.com>
2022-12-21 08:34:02 -0800

    size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,15 +4,14 @@
 
 class square():
     """initiallizing init function"""
-
+    
     def _int_(self, size=0):
-        """initiallizing size attribute and setting it to private
-
-        Args:
-        size(int): the size of the square
-        """
-        if (type(size) is not int):
-            raise TypeError("size must be an integer")
-        elif (size < 0):
-            raise ValueError("size must be >= 0")
-        self.__size = size
+    """initiallizing size attribute and making  it to private
+    Args:
+    size(int): the size of the square
+    """
+    if (type(size) is not int):
+        raise TypeError("size must be an integer")
+    elif (size < 0):
+        raise ValueError("size must be >= 0")
+    self.__size = size
