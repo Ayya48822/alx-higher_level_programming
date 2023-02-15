00328eea006442d8053551ff6e6a4dc901cff0e0
zainab ali <zali48822@gmail.com>
2023-02-15 08:07:44 -0800

    id can not be null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- creae table with i'd not null
-CREATE TABLE IF NOT EXISTS id_not_null (id INT default = 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT default 1, name VARCHAR(256));
