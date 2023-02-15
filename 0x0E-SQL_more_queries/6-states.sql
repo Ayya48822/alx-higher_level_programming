07c6878ad77e3b67b7bf61475832368c7bf13260
zainab ali <zali48822@gmail.com>
2023-02-15 08:44:02 -0800

    a table with primary key

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- create database with table in it
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`. states (id INT not null AUTO GENERATED, a primary key (id), name VARCHAR(256)not null);
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.states (id INT not null AUTO GENERATED, a primary key (id), name VARCHAR(256)not null);
